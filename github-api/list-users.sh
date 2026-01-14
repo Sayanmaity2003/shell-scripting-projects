337220383da821b2f4f3814c5c6738e83d24839b
Sayan Maity <125785306+Sayanmaity2003@users.noreply.github.com>
2026-01-14 11:16:03 +0530

    updated

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-# GitHub API URL
 API_URL="https://api.github.com"
 
-# GitHub username and personal access token
-USERNAME=$username
-TOKEN=$token
+# Fail if env variables are missing
+: "${username:?Environment variable username not set}"
+: "${token:?Environment variable token not set}"
 
-# User and Repository information
-REPO_OWNER=$1
-REPO_NAME=$2
+USERNAME="$username"
+TOKEN="$token"
 
-# Function to make a GET request to the GitHub API
+REPO_OWNER="$1"
+REPO_NAME="$2"
+
 function github_api_get {
     local endpoint="$1"
-    local url="${API_URL}/${endpoint}"
-
-    # Send a GET request to the GitHub API with authentication
-    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+    curl -s -u "${USERNAME}:${TOKEN}" \
+         -H "Accept: application/vnd.github+json" \
+         "${API_URL}/${endpoint}"
 }
 
-# Function to list users with read access to the repository
 function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
-    # Fetch the list of collaborators on the repository
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.permissions?.pull == true) | .login')"
+    collaborators=$(github_api_get "$endpoint" | jq -r '.[].login')
 
-    # Display the list of collaborators with read access
     if [[ -z "$collaborators" ]]; then
         echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
     else
@@ -35,8 +31,6 @@ function list_users_with_read_access {
         echo "$collaborators"
     fi
 }
-
-# Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
